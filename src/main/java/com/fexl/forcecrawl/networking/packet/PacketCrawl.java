a692e8348c62b83b17e0a4681a5a31c0eba813f1
Nathan Benson <nathanbenson750@gmail.com>
2024-07-02 06:52:04 -0600

    Fixed server loading from PacketCrawl being forced to client environment

--- src/main/java/com/fexl/forcecrawl/networking/packet/PacketCrawl.java
+++ src/main/java/com/fexl/forcecrawl/networking/packet/PacketCrawl.java
@@ -1,26 +1,17 @@
 package com.fexl.forcecrawl.networking.packet;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.networking.v1.PacketSender;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.client.server.LanServer;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.ServerGamePacketListenerImpl;
-import net.minecraft.world.entity.player.Player;
 
 import java.util.ArrayList;
 
 import com.fexl.forcecrawl.ForceCrawl;
 
-@Environment(EnvType.CLIENT)
 public record PacketCrawl(Boolean bool) implements CustomPacketPayload {
 	
 	public static ArrayList<ServerPlayer> crawlingPlayers = new ArrayList<ServerPlayer>();
